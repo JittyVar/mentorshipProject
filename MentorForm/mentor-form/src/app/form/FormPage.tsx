dcbda9ae0e929988ed269393d5545c3010931bf5
Alyssa <alyssapausanos1449@gmail.com>
2024-05-12 17:23:50 +1200

    remove image text

--- MentorForm/mentor-form/src/app/form/FormPage.tsx
+++ MentorForm/mentor-form/src/app/form/FormPage.tsx
@@ -7,7 +7,6 @@ const FormPage = () => {
     <Box>
       <Container>
         <a href="/api/auth/logout">Logout</a>
-        <h1>Form Page Image</h1>
         <VerticalLinearStepper />
       </Container>
     </Box>
