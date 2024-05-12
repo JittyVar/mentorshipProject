506dc8ecd6f411fc6156d734a9ae153acf2fda46
Alyssa <alyssapausanos1449@gmail.com>
2024-05-12 17:05:15 +1200

    remove textfield width

--- MenteeForm/mentee-form/src/components/subforms/goals/goals.tsx
+++ MenteeForm/mentee-form/src/components/subforms/goals/goals.tsx
@@ -85,7 +85,7 @@ const GoalsComponent = () => {
           }}
         />
         <div style={{ paddingBottom: "2%" }}>
-          <Typography sx={{ margin: "1%", width: "46ch" }}>
+          <Typography sx={{ margin: "1%" }}>
             What would you hope to get from this program?
           </Typography>
           <TextField
