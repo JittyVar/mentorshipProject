c273f002e1514fdea98f2ac10f240d840a0d016a
Alyssa <alyssapausanos1449@gmail.com>
2024-05-13 13:03:42 +1200

    add mentee proper pic

--- MenteeForm/mentee-form/src/app/form/page.tsx
+++ MenteeForm/mentee-form/src/app/form/page.tsx
@@ -2,7 +2,8 @@ import React from "react";
 //import RegistrationEnd from "./registrationEndedComponent";
 import VerticalLinearStepper from "@/components/verticalStepper";
 import { Box, Container } from "@mui/material";
-
+import Image from "next/image";
+import applicationpic from "../../../public/mentorshipapplication.png";
 // interface FormPageProps {
 //   isAuthenticated: boolean;
 // }
@@ -13,6 +14,7 @@ const FormPage = () => {
   return (
     <Box>
       <Container>
+        <Image src={applicationpic.src} alt="pic" width={850} height={200} />
         <VerticalLinearStepper />
       </Container>
     </Box>
