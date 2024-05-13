3b751c069dd3a000801204787638d6cbbdd3146a
Alyssa <alyssapausanos1449@gmail.com>
2024-05-13 12:52:22 +1200

    remove images for now

--- MenteeForm/mentee-form/src/app/form/page.tsx
+++ MenteeForm/mentee-form/src/app/form/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 //import RegistrationEnd from "./registrationEndedComponent";
 import VerticalLinearStepper from "@/components/verticalStepper";
 import { Box, Container } from "@mui/material";
-import Image from "next/image";
-import application from "../../../public/mentorshipapplication.png";
 
 // interface FormPageProps {
 //   isAuthenticated: boolean;
@@ -15,7 +13,6 @@ const FormPage = () => {
   return (
     <Box>
       <Container>
-        <Image src={application.src} alt="pic" width={850} height={250}></Image>
         <VerticalLinearStepper />
       </Container>
     </Box>
--- MentorForm/mentor-form/src/app/form/page.tsx
+++ MentorForm/mentor-form/src/app/form/page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import VerticalLinearStepper from "@/components/verticalStepper";
 import { Box, Container } from "@mui/material";
-import application from "../../../public/mentorshipapplication.png";
-import Image from "next/image";
 
 const FormPage = () => {
   return (
     <Box>
       <Container>
-        <Image src={application.src} alt="pic" width={850} height={250}></Image>
         <VerticalLinearStepper />
       </Container>
     </Box>
