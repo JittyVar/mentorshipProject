3643d2a17a141f815439e35089ee021a1e238ecc
JittyVar <89178940+JittyVar@users.noreply.github.com>
2024-07-15 08:21:14 +0000

    updated backend to only provide 3 top mentors

--- backend/tests/dummy-data-test/test app v1 dummy data.py
+++ backend/tests/dummy-data-test/test app v1 dummy data.py
@@ -229,19 +229,24 @@ final_df = merged_df[['unique_id','fullName', 'mentor_fullName','predicted_cosin
 
 # Print the sorted DataFrame
 print("The Initial Output")
-print(final_df.head())
+#print(final_df.head())
+print(final_df)
 
-final_df.to_csv('proposed_matches.csv', index=False)
+#final_df.to_csv('proposed_matches.csv', index=False)
 
 # Sort the final_df DataFrame by 'fullName' and 'predicted_cosine_similarity' in descending order
-#final_df = final_df.sort_values(by=['fullName', 'predicted_cosine_similarity'], ascending=[True, False])
+final_df = final_df.sort_values(by=['fullName', 'predicted_cosine_similarity'], ascending=[True, False])
 
-# Remove duplicate mentee names
-#final_df = final_df.drop_duplicates(subset='fullName', keep='first')
+# Group the DataFrame by 'fullName'
+grouped_df = final_df.groupby('fullName')
 
-# Create a new DataFrame called 'mentors'
-#mentors = final_df[['unique_id', 'fullName', 'mentor_fullName', 'predicted_cosine_similarity']]
+# Select the top 3 rows for each group
+top_3_df = grouped_df.head(3)
 
-# Print the 'mentors' DataFrame
-#print("Final Dataset")
-#print(mentors.head())
+# Print the top 3 DataFrame
+print("Top 3 Mentors per Mentee:")
+print(top_3_df)
+top_3_df.to_csv('proposed_matches.csv', index=False)
+
+
+
